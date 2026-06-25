3c00bc43bea9a896cc3a0d137a0cdcd564fcdb1a
simantopal <simantop13@gmail.com>
2026-06-25 19:37:42 +0600

    error fixing

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,25 +6,29 @@ const client = new MongoClient(process.env.MONGO_DB_URI);
 const db = client.db('ticket-bari');
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+
   emailAndPassword: {
     enabled: true,
   },
+
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    }
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
   },
+
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
-    client
+    client,
   }),
+
   user: {
     additionalFields: {
       role: {
         type: "string",
         defaultValue: "passenger",
-      }
-    }
+      },
+    },
   },
 });
